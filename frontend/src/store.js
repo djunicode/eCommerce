cf584b582f0b12110d948ec121e0cf6c687523b9
dhirajssh <60935022+dhirajssh@users.noreply.github.com>
2021-04-22 11:35:35 +0530

    merge conflicts3

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/named */
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
