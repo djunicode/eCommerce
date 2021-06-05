0ecc99adb349783e430d1b05707d24857503ab10
Jazib Dawre <jazib980@gmail.com>
2021-06-05 23:13:44 +0530

    Integrate push notification with backend

---
+++ Notification Demo Frontend/index.js
@@ -0,0 +1,129 @@
+var subscriptionButton = document.getElementById('subscriptionButton');
+var notificationButton = document.getElementById('notificationButton');
+
+// change the authtoken variable in line 51,71,110 according to redux state
+var authtoken =
+	'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYwMmU1Yjc5MDk2NzE3MjMzMGU5ZWJhYyIsImlhdCI6MTYyMjkxMzUwMiwiZXhwIjoxNjIyOTE3MTAyfQ.VMcocQLJrzByq64n_OqkpUmRL860FJgcHKkh8Tcd5TY';
+
+navigator.serviceWorker.register('service-worker.js');
+
+navigator.serviceWorker.ready
+	.then((registration) => {
+		console.log('service worker registered');
+		subscriptionButton.removeAttribute('disabled');
+
+		return registration.pushManager.getSubscription();
+	})
+	.then((subscription) => {
+		if (subscription) {
+			console.log('Already subscribed', subscription.endpoint);
+			setUnsubscribeButton();
+		} else {
+			setSubscribeButton();
+		}
+	});
+
+const urlBase64ToUint8Array = (base64String) => {
+	const padding = '='.repeat((4 - (base64String.length % 4)) % 4);
+	const base64 = (base64String + padding)
+		.replace(/-/g, '+')
+		.replace(/_/g, '/');
+
+	const rawData = window.atob(base64);
+	const outputArray = new Uint8Array(rawData.length);
+
+	for (let i = 0; i < rawData.length; ++i) {
+		outputArray[i] = rawData.charCodeAt(i);
+	}
+	return outputArray;
+};
+
+const subscribe = () => {
+	navigator.serviceWorker.ready
+		.then(async (registration) => {
+			const response = await fetch(
+				'http://localhost:5000/notifications/vapidPublicKey',
+				{
+					method: 'GET',
+					withCredentials: true,
+					credentials: 'include',
+					headers: {
+						Authorization: 'Bearer ' + authtoken,
+					},
+				}
+			);
+			const vapidPublicKey = await response.text();
+
+			const convertedVapidKey = urlBase64ToUint8Array(vapidPublicKey);
+
+			return registration.pushManager.subscribe({
+				userVisibleOnly: true,
+				applicationServerKey: convertedVapidKey,
+			});
+		})
+		.then((subscription) => {
+			console.log('Subscribed', subscription.endpoint);
+			return fetch('http://localhost:5000/notifications/register', {
+				method: 'POST',
+				withCredentials: true,
+				credentials: 'include',
+				headers: {
+					Authorization: 'Bearer ' + authtoken,
+					'Content-Type': 'application/json',
+				},
+				body: JSON.stringify(subscription),
+			});
+		})
+		.then(setUnsubscribeButton);
+};
+
+const unsubscribe = () => {
+	navigator.serviceWorker.ready
+		.then((registration) => {
+			return registration.pushManager.getSubscription();
+		})
+		.then((subscription) => {
+			console.log('Unsubscribed', subscription.endpoint);
+			return subscription.unsubscribe();
+		})
+		.then(setSubscribeButton);
+};
+
+const notify = () => {
+	navigator.serviceWorker.ready
+		.then((registration) => {
+			return registration.pushManager.getSubscription();
+		})
+		.then((subscription) => {
+			if (!subscription) {
+				console.log('Please subscribe first');
+				return;
+			}
+			console.log('Notification sent');
+			return fetch(
+				'http://localhost:5000/notifications/sendNotification',
+				{
+					method: 'POST',
+					withCredentials: true,
+					credentials: 'include',
+					headers: {
+						Authorization: 'Bearer ' + authtoken,
+						'Content-Type': 'application/json',
+					},
+					body: JSON.stringify({ message: 'Hi this is Jazib!' }),
+				}
+			);
+		});
+};
+
+notificationButton.onclick = notify;
+
+const setSubscribeButton = () => {
+	subscriptionButton.onclick = subscribe;
+	subscriptionButton.textContent = 'Subscribe!';
+};
+
+const setUnsubscribeButton = () => {
+	subscriptionButton.onclick = unsubscribe;
+	subscriptionButton.textContent = 'Unsubscribe!';
+};
