ab30d97afb8807afdf1d4ae87157313215894bd5
Jazib Dawre <jazib980@gmail.com>
2021-06-05 23:10:13 +0530

    setup cors for web push

--- backend/server.js
+++ backend/server.js
@@ -47,12 +47,10 @@ if (process.env.NODE_ENV === 'development') {
 }
 
 app.use(express.json());
-app.use(cors({ credentials: true }));
+app.use(cors({ credentials: true, origin: true }));
 app.use(verify);
-app.use(cors());
 
 app.use(function (req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
   res.header(
     'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept'
