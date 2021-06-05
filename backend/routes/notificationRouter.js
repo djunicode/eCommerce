435b6743b2e46293ee78d6814f72ceb7f15f9aee
Jazib Dawre <jazib980@gmail.com>
2021-06-05 17:41:59 +0530

    Add controller to route

--- backend/routes/notificationRouter.js
+++ backend/routes/notificationRouter.js
@@ -2,11 +2,13 @@ import express from 'express';
 import {
   getPublicKey,
   registerUser,
+  sendNotification,
 } from '../controllers/notificationController.js';
 
 const notificationRouter = express.Router();
 
 notificationRouter.route('/getVapidPublicKey').get(getPublicKey);
 notificationRouter.route('/register').post(registerUser);
+notificationRouter.route('/sendNotification').post(sendNotification);
 
 export default notificationRouter;
